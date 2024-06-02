abf2f019648ee9ee048e6f2195965511d70cbf2b
Kaike <kaikedias98@gmail.com>
2024-06-02 20:46:26 -0300

    feat: add transactionDto

---
+++ src/main/java/com/example/desafiopicpay/dtos/TransactionDto.java
@@ -0,0 +1,10 @@
+package com.example.desafiopicpay.dtos;
+
+import java.math.BigDecimal;
+
+public record TransactionDto(
+        BigDecimal value,
+        Long senderId,
+        Long receiverId
+) {
+}
